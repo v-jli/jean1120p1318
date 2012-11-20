8be1078c0fd9b6ecd6816e8e522f5390cad9f639
jean  is author <dnpxeimme@hotmail.com>
2012-11-20 15:06:40 +0800

    aa

--- MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace MvcApplication1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "My awesome ASP.NET MVC application with changes.jean well done";
+            ViewBag.Message = "My awesome ASP.NET MVC application with changes.";
 
             return View();
         }
