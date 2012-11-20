131430899af1e8bbcf5b41f59f68d415c5b66b7c
jean  is author <dnpxeimme@hotmail.com>
2012-11-20 13:41:08 +0800

    aa

---
+++ MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    //public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "My awesome ASP.NET MVC application with changes.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your quintessential app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your quintessential contact page.";
+
+            return View();
+        }
+    }
+}
